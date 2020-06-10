cfa02fe720ca35c80627bcf2411201ff2ae89ffc
Trevor Stubbs <stubbste@gmail.com>
2020-06-10 16:55:08 -0700

    staged for update

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations(
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude VARCHAR(255),
+  longitude VARCHAR(255)
+);
+
+INSERT INTO locations (
+  search_query, 
+  formatted_query, 
+  latitude,
+  longitude
+) 
+VALUES (
+  'seattle', 
+  'Seattle, King County, Washington, USA',
+  '47.6038321',
+  '-122.3300624'
+  );
+SELECT * FROM locations;
