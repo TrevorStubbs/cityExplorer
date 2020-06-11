4a6d96e33965dc435b5a23b5d6690156039d206d
Trevor Stubbs <stubbste@gmail.com>
2020-06-10 19:49:51 -0700

    database hooked up

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 // Import and configure the .env
 require('dotenv').config();
 
-// Import pg
-const pg = require('pg');
-
 // import cors
 const cors = require('cors');
 app.use(cors());
+
+// Import pg
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
 
 //Import superagent
 const superagent = require('superagent');
@@ -26,12 +28,50 @@ app.get('/', (request, response) => {
   response.status(200).send('Am I on the browser?');
 });
 
+/////-----------WIP---------
+
+// client is going to ask for a location
+// check the database if its there
+    // if its there return that info into the constructor
+    // if its not there do the superagent call and then pass it into the constructor and it to the database
+// app.get('/add', (request, response) => {
+//   // collect information to add to our database
+//   console.log('on the add route', request.query);
+//   let search_query = 'lynnwood'
+//   let formatted_query = 'Lynnwood, Snohomish County, Washington, USA'
+//   let latitude = '33.924831';
+//   let longitude = '-118.2024154';
+
+//   let sqlQuery = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);'; // need 2 ;
+//   let safeValue = [search_query, formatted_query, latitude, longitude];
+
+//   client.query(sqlQuery, safeValue)
+//     .then(() => {})
+//     .catch()
+// })
+
+app.get('/select', (request, response) => {
+// see everyone in the database
+//-------------------Test-------------------------
+  console.log('1st here');
+  let sqlQuery = 'SELECT * FROM locations;';
+
+  console.log('2nd here');
+  client.query(sqlQuery)
+    .then(sqlResults => {
+      console.log('3rd HERE?', sqlResults.rowCount);
+      response.status(200).send(sqlResults.rows);
+    })
+    .catch()
+});
+
 // Location route
 app.get('/location', (request, response) => {
   try{
     // get the city the user requested
     let city = request.query.city;
 
+    
     // grab the url and put in the API key
     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
 
@@ -142,7 +182,10 @@ function Trail(obj){
 }
 
 // Start the server
-app.listen(PORT, () =>{
-  console.log(`listening on ${PORT}.`);
-});
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    })
+  })
 
